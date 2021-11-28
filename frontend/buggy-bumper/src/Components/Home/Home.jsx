271a7fb1195a696b2681efa73cbcbaf448ac2d7b
pineda.juan92 <pineda.juan92@gmail.com>
2021-11-28 14:03:04 -0500

    Styles for users home page

--- frontend/buggy-bumper/src/Components/Home/Home.jsx
+++ frontend/buggy-bumper/src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import homeCarImage from "../../assets/circle.png";
+import homeCircleImage from "../../assets/circle.png";
 import axios from "axios";
-import { Card, Button, ListGroupItem } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { API_URL } from "../../constants";
 
 import "./Home.scss";
@@ -26,7 +26,7 @@ const Home = () => {
           </h1>
           <h2>LA MEJOR RED DE ALQUILER DE AUTOS</h2>
         </section>
-        <img src={homeCarImage} alt="Buggy Bumper Home car " />
+        <img src={homeCircleImage} alt="Buggy Bumper Home car" />
       </div>
       <div className="cars">
         <h1>Alquila facilmente nuestros autos</h1>
@@ -36,14 +36,25 @@ const Home = () => {
               <Card.Img variant="top" src={image} />
               <Card.Body>
                 <Card.Title>{carType}</Card.Title>
-                <ListGroupItem>{doors}</ListGroupItem>
-                <ListGroupItem>{seats}</ListGroupItem>
-                <ListGroupItem>{bags}</ListGroupItem>
+                <ul>
+                  <li>{doors}</li>
+                  <li>{seats}</li>
+                  <li>{bags}</li>
+                </ul>
                 <h5>${price}</h5>
                 <Button variant="primary">Alquilar</Button>
               </Card.Body>
             </Card>
           ))}
+        </section>
+        <section className="commercial-info">
+          <p>
+            Mejor Agencia de alquiler y renta de carros y autos en Medellín y
+            Antioquia, en todas las gamas que van desde autos Económicos, Gama
+            Baja, Gama Alta, Sedan, Alquiler de Camionetas, Blindados y Vans
+          </p>
+          <img src={homeCircleImage} alt="Buggy Bumper Home car" />
+          <div className="sombra"></div>
         </section>
       </div>
     </>
